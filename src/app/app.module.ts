5a0c03ed9632ab610a5b8d69bebe8957d6858ce0
purushottam pachange <purushottampachnage@gmail.com>
2025-12-22 17:08:43 +0530

    added btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductComponent2 } from './product2/product2.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -11,10 +13,13 @@ import { ProductComponent2 } from './product2/product2.component';
     AppComponent,
     ProductComponent,
     ProductComponent2,
-   
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
